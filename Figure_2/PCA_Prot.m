1e2cb826b5f9f334886390455a695f58bf8d4fde
DanielSprockett <danielsprockett@gmail.com>
2014-06-06 13:48:01 -0700

    Dan

--- Figure_2/PCA_Prot.m
+++ Figure_2/PCA_Prot.m
@@ -1,43 +1,50 @@
 
-% By Emily Alsentzer
-clear all
-close all hidden
-clc
-load('axes140523.mat');
-load('normOverlordFinal_140523.mat');
-load('GOenrichMat'); % 3013 * 3 * 3 * 5 4d matrix NOTE: This isn't the most updated version!
+% Figure 2
+
+% Figure 2.A.
+% This script runs a Principal Component Analysis on the proteomic database
+% and generations a PCA Plot.
+
+% Load Initialized Data
+clear all;
+close all hidden;
+clc;
+load('../Initialization/axes.mat');
+load('../Initialization/normOverlordFinal.mat');
 all_samples = [];
 all_labels = {};
-normOverlord = normOverlordFinal; 
 
-% This loops through all mouse #s, colonization states, and locations to
-% create a 2D matrix where each row is a gocode_id and each column is a
-% different sample (sample A, B, etc.). The matrix, all_samples, contains the abundance
-% of each GO code at each sample.  The all_labels matrix is a 1x45 matrix
-% that contains labels for each sample in the order that they are in teh
-% all_samples matrix. 
-for mouse_num = 1:3
-    for colonization = 1:3
-        for loc = 1:5
-            all_samples = [all_samples GOenrichMat(:,mouse_num, colonization, loc)];
+% Loops through all mouse replicates, colonization states, and locations to
+% create a 2D matrix where each row is a protein_id and each column is a
+% different sample (sample A, B, etc.). The matrix, all_samples, contains 
+% the abundance of each protein at each sample.  
+% The all_labels matrix is a 1x45 matrix
+% that contains labels for each sample in the order that they are in the
+% all_samples matrix. The all_labels matrix and the proteins matrix can be 
+% used for labeling the axes of any plots.
+
+for mouse_num = 1:3;
+    for colonization = 1:3;
+        for loc = 1:5;
+            all_samples = [all_samples normOverlordFinal(:,mouse_num, colonization, loc)];
             label = strcat(axes{2}{mouse_num}, '_', axes{3}{colonization} , '_', axes{4}{loc});           
             all_labels = [all_labels label];
-        end
-    end
-end
-
+        end;
+    end;
+end;
+            
 
 %% PCA 
-% By Daniel Sprockett
+
 % step 1: Calculate the average over all vectors
 avg = mean(all_samples);
 
 %%
-number_of_GOcodes = size(all_samples,1);
+number_of_proteins = size(all_samples,1);
 number_of_samples = size(all_samples,2);
 
 % step 2: Subtract the average from all vectors
-diff_avg = all_samples-repmat(avg, number_of_GOcodes, 1);
+diff_avg = all_samples-repmat(avg, number_of_proteins, 1);
 
 %%
 % step 3: Calculate the covariance_matrixariance matrix
@@ -46,11 +53,11 @@ covariance_matrix = zeros(number_of_samples, number_of_samples);
 
 for i = 1:number_of_samples;
     for j = 1:number_of_samples;
-        covariance_matrix(i,j) = 1/(number_of_GOcodes - 1) * ...
+        covariance_matrix(i,j) = 1/(number_of_proteins - 1) * ...
             sum(diff_avg(:,i).*diff_avg(:,j));
-    end
-end
-figure
+    end;
+end;
+figure;
 imagesc(covariance_matrix);
 colorbar;
 
@@ -89,8 +96,8 @@ color_by_GutRegion = zeros(45, 3);
     else strcmp(split_strings(i,3), 'Stomach'); % Color Stomach magenta
     color_by_GutRegion(i,1) = 1; color_by_GutRegion(i,2) = 0; color_by_GutRegion(i,3) = 1;
     
-    end
- end
+    end;
+ end;
  
 % Colonization State
 
@@ -110,15 +117,15 @@ color_by_ColonizationState = zeros(45, 3);
     end
  end
 
- 
+
 %%
 
-% Plot PC1 vs PC2 vs PC3 for Gut Region
+% Plot PC1 vs PC2 vs PC3 for Gut Location
 
 figure
 scatter3(eigenvectors(:,1), eigenvectors(:,2), eigenvectors(:,3), 100, color_by_GutRegion, 'filled');
-hold on
-%title('GO Code Principal Component Analysis: Gut Region');
+hold on;
+title('Principal Component Analysis: Proteins by Gut Location');
 xlabel(sprintf('PC1 = %.0f%%', explained(1,1)));
 ylabel(sprintf('PC2 = %.0f%%', explained(2,1)));
 zlabel(sprintf('PC3 = %.0f%%', explained(3,1)));
@@ -126,53 +133,7 @@ zlabel(sprintf('PC3 = %.0f%%', explained(3,1)));
 I = length(split_strings(:,3)) - I(length(I):-1:1); 
 p = findobj(gca,'Type','Patch');
 legend(p(I), 'stomach', 'prox colon', 'jejunum', 'ileum', 'cecum');
-hold off
-
-%%
-
-% Plot PC1 vs PC2 for Gut Region
-
-figure
-scatter(eigenvectors(:,1), eigenvectors(:,2), 100, color_by_GutRegion, 'filled');
-hold on;
-%title('Principal Component Analysis: Gut Location');
-xlabel(sprintf('PC1 = %.0f%%', explained(1,1)));
-ylabel(sprintf('PC2 = %.0f%%', explained(2,1)));
-[~, I] = unique(split_strings(:,3)); 
-I = length(split_strings(:,3)) - I(length(I):-1:1); 
-p = findobj(gca,'Type','Patch');
-legend(p(I), 'stomach', 'prox colon', 'jejunum', 'ileum', 'cecum', 'Location','NorthWest');
 hold off;
 
-%%
-% Plot PC2 vs PC3 for Gut Region
+print -dpdf -r600 Figure_2A.pdf
 
-figure
-scatter(eigenvectors(:,2), eigenvectors(:,3), 100, color_by_GutRegion, 'filled');
-hold on;
-%title('Principal Component Analysis: Gut Location');
-xlabel(sprintf('PC2 = %.0f%%', explained(2,1)));
-ylabel(sprintf('PC3 = %.0f%%', explained(3,1)));
-[~, I] = unique(split_strings(:,3)); 
-I = length(split_strings(:,3)) - I(length(I):-1:1); 
-p = findobj(gca,'Type','Patch');
-%legend(p(I), 'stomach', 'prox colon', 'jejunum', 'ileum', 'cecum');
-hold off;
-
-
-%%
-
-% Plot PC1 vs PC2 vs PC3 for Colonization State
-
-figure
-scatter3(eigenvectors(:,1), eigenvectors(:,2), eigenvectors(:,3), 100, color_by_ColonizationState, 'filled');
-hold on
-title('Principal Component Analysis: Colonization State');
-xlabel(sprintf('PC1 = %.0f%%', explained(1,1)));
-ylabel(sprintf('PC2 = %.0f%%', explained(2,1)));
-zlabel(sprintf('PC3 = %.0f%%', explained(3,1)));
-[~, I] = unique(split_strings(:,2)); 
-I = length(split_strings(:,2)) - I(length(I):-1:1); 
-p = findobj(gca,'Type','Patch');
-legend(p(I), 'BT', 'RF', 'GF');
-hold off
